914e3a28ce278c032653c9bfafa189fdd3b724ef
PJZ9n <pjz9n@yahoo.co.jp>
2020-03-09 18:56:39 +0900

    Hello World

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+
+def print_hello():
+    print("Hello")
+
+
+def print_world():
+    print("World")
+
+
+print_hello()
+
+print_world()
+
+for i in range(5):
+    print_hello()
+    print_world()
